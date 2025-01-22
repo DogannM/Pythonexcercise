ef22bd0f0bfc8b211a179b4a9db9fa1ad3bdbda1
DogannM <canmrnc2620@gmail.com>
2025-01-22 23:33:45 +0300

    The first excercise has been  completed.

---
+++ excerise.py
@@ -0,0 +1,35 @@
+#Beginner-Level Exercises
+#Hello, World!
+print("hello, world!")
+#Write a program that prints "Hello, World!" to the screen.
+print("hello, world!")
+#Modify it to ask for the user's name and greet them.
+greetings = input("What is your name? :")
+print("Greetings, " + greetings + "!") 
+
+#Simple Arithmetic
+#Create a program that takes two numbers from the user and performs addition, subtraction, multiplication, and division.
+var1 = input("Enter a number: ")
+var2 = input("Enter another number: ")
+
+if type(var1) is int and type(var2) is int == True :
+    pass
+else:
+    print("Please enter a number")
+    var1 = input("Enter a number not a string ! : ") 
+    var2 = input("Enter another number not a string ! : ")
+
+print("Addition: " + str(var1 + var2))
+print("Subtraction: " + str(var1 - var2))
+print("Multiplication: " + str(var1 * var2))
+print("Division: " + str(var1 / var2))
+
+#Odd or Even
+#Write a program that asks the user for a number and prints whether it is odd or even.
+
+#Basic Calculator
+#Create a calculator that performs basic operations (add, subtract, multiply, divide) based on user input.
+
+
+#Guess the Number
+#Generate a random number between 1 and 100 and let the user guess it, giving hints (higher/lower) until they get it right.
